7686561e1c7280cebaae84c632f2dea7e437db4f
vitaZ-dev <vitaminzdev@gmail.com>
2023-05-25 17:24:03 +0900

    feat: 헤더 반응형 및 일반 작업 완료

--- js/header.js
+++ js/header.js
@@ -1,41 +1,34 @@
 window.addEventListener("load", function () {
-  // nav 에 마우스 hover하면 header 높이 260px로 변화주기
-  // nav 에 마우스 out  하면 header 높이 100px로 변화주기
-
-  // header 를 js로 저장해 보자(변수 정의해 보자)
   const header = document.querySelector(".header");
-
-  // nav    를 js로 저장해 보자(변수 정의해 보자)
   let nav = document.querySelector(".nav");
-
-  // undefined 만 아니면 된다
-  // console.log(header);
-  // console.log(nav);
-  // + null = 값이 없음. 그냥 없음.
-
-  // nav 에 마우스 hover 처리
-  // html 요소에 마우스 hover 처리하는 법
   nav.addEventListener("mouseenter", function () {
-    // header의 높이를 260px로 변경하고 싶다
-    // header.style.height = "260px";
-
-    // 260px 로 변경되는 class를 작성해두었다
     header.classList.add("header-active");
   });
-  // nav 에 마우스 out 처리
-  // html 요소에 마우스 out 처리하는 법
   nav.addEventListener("mouseleave", function () {
-    // header의 높이를 100px로 변경하고 싶다
-    // header.style.height = "100px";
-
-    // 260px 로 변경되는 class를 제거한다
     header.classList.remove("header-active");
   });
-  // 지금처럼 숫자로 css 변경이 가능하다
-  // 하지만 비추천하는 방법이었다!!!
 
-  // 클래스를 추가했다가 제거했다가 하는 방법이 더 좋다!
-  // js를 안 고치고 css만 고치면 되니까 더 좋다!
+  let gnbA = document.querySelectorAll(".gnb >li");
+  let navBlueBar = document.querySelector(".nav-blue-bar");
+  gnbA.forEach((item) => {
+    // 최초 위치 및 너비
+    let posX = gnbA[0].getBoundingClientRect().left;
+    let widthX = gnbA[0].getBoundingClientRect().width;
+    navBlueBar.style.left = posX + "px";
+    navBlueBar.style.width = widthX + "px";
 
-
+    item.addEventListener("mouseenter", () => {
+      let posX = item.getBoundingClientRect().left;
+      let widthX = item.getBoundingClientRect().width;
+      // navBlueBar.style.left = posX + "px";
+      // navBlueBar.style.width = widthX + "px";
+      anime({
+        targets: navBlueBar,
+        left: posX,
+        width: widthX,
+        easing: "easeInOutQuad",
+        duration: 500,
+      });
+    });
+  });
 });
--- js/mbnav.js
+++ js/mbnav.js
@@ -17,6 +17,10 @@ window.addEventListener("load", function () {
       mbNav.classList.remove(mbNavActive);
       mbWrap.classList.remove(mbNavWrap);
     }
+    // reset 하겠다
+    resetSubMenu();
+    // 펼침 기록 변수 초기화
+    sidOpenNumber = undefined;
 
     //화면의 리사이즈를 체크해서 처리
     window.addEventListener("resize", function () {
@@ -25,32 +29,97 @@ window.addEventListener("load", function () {
       if (windowWIdth > mbWidth) {
         mbWrap.classList.remove(mbNavWrap);
         mbNav.classList.remove(mbNavActive);
+        // reset 하겠다
+        resetSubMenu();
+        // 펼침 기록 변수 초기화
+        sidOpenNumber = undefined;
+      }
+    });
+
+    /*
+		// mbWrap에 조건문을 안 줬을 땐 이 코드를 넣으면 됨
+		const mbGnb = document.querySelector("mb-gnb");
+    mbGnb.addEventListener("click", (event) => {
+      // 이벤트 전달 막음
+      event.stopPropagation();
+    });
+		*/
+    // dim 영역 클릭 시 내비게이션 닫기
+    mbWrap.addEventListener("click", function (event) {
+      if (event.target === event.currentTarget) {
+        mbNav.classList.remove(mbNavActive);
+        mbWrap.classList.remove(mbNavWrap);
+        // reset 하겠다
+        resetSubMenu();
+        // 펼침 기록 변수 초기화
+        sidOpenNumber = undefined;
       }
     });
   });
 
   // 아코디언 메뉴
+  const sideLis = document.querySelectorAll(".side-menu > li");
   const sideMenuA = document.querySelectorAll(".side-menu > li > a");
   const sideSubMenu = document.querySelectorAll(".side-menu > li > .submenu");
+  const sideMenuOffset = 53;
+  // 펼쳐질 높이값을 담아둔다
+  const sideOpenHeightArr = [];
+  let sidOpenNumber;
 
+  // 각 서브 메뉴의 높이
+  sideSubMenu.forEach((item, index) => {
+    // sideOpenHeightArr.push(item.scrollHeight);
+    sideOpenHeightArr[index] = item.scrollHeight + sideMenuOffset;
+  });
+
+  // 클릭 처리
   sideMenuA.forEach((item, index) => {
-    item.addEventListener("click", (event) => {
-      event.preventDefault(); // a태그 기본동작 막음
-      changeSubmenu(index);
+    item.addEventListener("click", function (event) {
+      // 기본동작 href 막기
+      event.preventDefault();
+      showSubMenu(index);
     });
   });
 
-  function changeSubmenu(_index) {
-    sideSubMenu.forEach((item, index) => {
-      if (_index === index) {
-        if (item.style.display === "block") {
-          item.style.display = "none";
-        } else {
-          item.style.display = "block";
-        }
-      } else {
-        item.style.display = "none";
+  function resetSubMenu() {
+    sideLis.forEach((item) => {
+      // item.style.height = sideMenuOffset + "px";
+      anime({
+        targets: item,
+        height: 53,
+        easing: "easeInOutQuad",
+        duration: 300,
+      });
+    });
+  }
+  function showSubMenu(_showNumber) {
+    // 모든 li 의 높이를 53으로 하겠다
+    // reset 하겠다
+    resetSubMenu();
+
+    // 펼친 번호와 같은 값이 인자로 전달되면
+    // 펼침 코드를 실행하지 않는다.
+    if (_showNumber === sidOpenNumber) {
+      // 펼침 기록 변수 초기화
+      sidOpenNumber = undefined;
+      // 함수중단
+      return;
+    }
+
+    // li 의 높이를 변경하겠다
+    sideLis.forEach((item, index) => {
+      if (_showNumber === index) {
+        // item.style.height = `${sideOpenHeightArr[_showNumber]}px`;
+        anime({
+          targets: item,
+          height: sideOpenHeightArr[_showNumber],
+          easing: "easeInOutQuad",
+          duration: 300,
+        });
       }
     });
+
+    // 페이지 저장
+    sidOpenNumber = _showNumber;
   }
 });
