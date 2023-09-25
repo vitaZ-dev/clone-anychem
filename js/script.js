a7bcb9d0929ae56439614b6831f4b15255a8568d
vitaZ-dev <vitaminzdev@gmail.com>
2023-09-25 14:50:44 +0900

    feat: 클론코딩 안내창 추가

--- js/script.js
+++ js/script.js
@@ -2,4 +2,14 @@ window.addEventListener("load", function () {
   this.window.addEventListener("scroll", function () {
     AOS.init();
   });
+
+  // 모달창 처리
+  const bodyTag = document.querySelector("body");
+  bodyTag.classList.add("modal-active");
+  const modal = document.querySelector(".modal");
+  const modalCont = document.querySelector(".modal-cont");
+  modalCont.onclick = function () {
+    bodyTag.classList.remove("modal-active");
+    modal.style.display = "none";
+  };
 });
